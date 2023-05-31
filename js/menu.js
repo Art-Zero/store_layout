e5fe4175ac55e3c2fa36dac3108fb3b5588ed16d
Art_Zer0 <127736507+Art-Zero@users.noreply.github.com>
2023-05-31 15:10:05 +0500

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".nav-btn");
+const img_navBtn = document.querySelector(".img_nav-btn");
+
+navBtn.onclick = () => {
+    if(nav.classList.toggle("open")) {
+        img_navBtn.src = "../img/icon/close-menu-white.png";
+    } else {
+        img_navBtn.src = "../img/icon/open-menu-white.png";
+    }
+}
